3fa52cf7566bd629ac68ce69cc3bb9f323d36281
Student-01-Jason <63417122+Student-01-Jason@users.noreply.github.com>
2020-06-11 01:38:48 +0100

    Update test_person.py

--- test_person.py
+++ test_person.py
@@ -2,3 +2,9 @@ import unittest
 from person import Person
 
 # TODO
+class TestPerson(unittest.TestCase):
+
+    def setUp(self):
+        print('Test start.')
+        self.p1 = Person('Bill', 'Gates', 15)
+        self.p2 = Person('Steve', 'Jobs', 25)
